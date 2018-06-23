a13550d525e1b7ee989de4514453b6214fa55c79
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-06-23 12:55:07 -0500

    Copied file in

---
+++ FreshInstallPack/app_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+sudo eopkg install vim -y
+sudo eopkg install screenfetch -y
+sudo eopkg install steam -y
+sudo eopkg install gimp -y
+sudo eopkg install bleachbit -y
+sudo eopkg install virtualbox -y
+sudo eopkg install notepadqq -y
+sudo eopkg install wine -y
+
+
+sudo eopkg install lightdm-gtk-greeter-settings -y
+
+
+
