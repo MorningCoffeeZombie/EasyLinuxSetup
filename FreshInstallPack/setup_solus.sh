277ab2d7ea32ede088d5e38c677342e9e68d9fbd
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-06-23 17:02:48 -0500

    Install nano

--- FreshInstallPack/setup_solus.sh
+++ FreshInstallPack/setup_solus.sh
@@ -16,7 +16,7 @@ echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
 echo Home wifi password is: YJRbLRRhPQ8844
 
 # reminders
-echo change the password to something easier with passwd $USER
+echo change the password to something easier with passwd
 echo if "clr-boot-manager update" has issues, add a line like this to fstab:
 echo UUID=1FE3-4160 /boot vfat defaults 0 0 
 echo I have already created a backup of fstab your desktop at /home/fstab.BAK
@@ -26,6 +26,7 @@ echo Once /boot is mounted, run DoFlicky (Solus hardware detector) to install nv
 # update and install things
 eopkg up
 eopkg install vim
+eopkg install nano
 
 # have user check kernel and drivers
 inxi -Fx
