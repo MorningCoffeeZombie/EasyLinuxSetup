e8ba4facc1c5806f393e32cf014b7905aa25278c
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-06-30 23:17:28 -0500

    Added support to auto detect kernel series

--- FreshInstallPack/app_installer.sh
+++ FreshInstallPack/app_installer.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
 
+# LTS v. Current kernels
+# https://solus-project.com/articles/troubleshooting/boot-management/en/#installing-an-alternative-kernel
+
+
+# LTS Kernel
+if [[ $(uname -r) = *lts* ]]; then
+  echo "LTS kernel detected"
+sudo eopkg install bbswitch -y
+sudo eopkg install broadcom-sta -y
+sudo eopkg install linux-lts-headers -y
+sudo eopkg install nvidia-304-glx-driver -y
+sudo eopkg install nvidia-340-glx-driver -y
+sudo eopkg install nvidia-glx-driver -y
+sudo eopkg install razer-drivers -y
+sudo eopkg install v4l2loopback -y
+sudo eopkg install vhba-module -y
+sudo eopkg install virtualbox-common -y
+fi
+
+
+# Current Kernel
+if [[ $(uname -r) = *current* ]]; then
+  echo "Current kernel detected"
+sudo eopkg install bbswitch-current -y
+sudo eopkg install broadcom-sta-current -y
+sudo eopkg install linux-current-headers -y
+sudo eopkg install nvidia-304-glx-driver-current -y
+sudo eopkg install nvidia-340-glx-driver-current -y
+sudo eopkg install nvidia-glx-driver-current -y
+sudo eopkg install razer-drivers-current -y
+sudo eopkg install v4l2loopback-current -y
+sudo eopkg install vhba-module-current -y
+sudo eopkg install virtualbox-current -y
+fi
+
+
+
 # All kernels
 sudo eopkg install linux-headers -y
 sudo eopkg install nano -y
@@ -14,40 +51,9 @@ sudo eopkg install wine -y
 sudo eopkg install grsync -y
 sudo eopkg install git -y
 
+
 # Non-essential settings configurator
 #sudo eopkg install lightdm-gtk-greeter-settings -y
-
-# LTS v. Current kernels
-# https://solus-project.com/articles/troubleshooting/boot-management/en/#installing-an-alternative-kernel
-
-
-# LTS Kernel
-sudo eopkg install bbswitch -y
-sudo eopkg install broadcom-sta -y
-sudo eopkg install linux-lts-headers -y
-sudo eopkg install nvidia-304-glx-driver -y
-sudo eopkg install nvidia-340-glx-driver -y
-sudo eopkg install nvidia-glx-driver -y
-sudo eopkg install razer-drivers -y
-sudo eopkg install v4l2loopback -y
-sudo eopkg install vhba-module -y
-sudo eopkg install virtualbox-common -y
-
-
-
-
-
-# Current Kernel
-sudo eopkg install bbswitch-current -y
-sudo eopkg install broadcom-sta-current -y
-sudo eopkg install linux-current-headers -y
-sudo eopkg install nvidia-304-glx-driver-current -y
-sudo eopkg install nvidia-340-glx-driver-current -y
-sudo eopkg install nvidia-glx-driver-current -y
-sudo eopkg install razer-drivers-current -y
-sudo eopkg install v4l2loopback-current -y
-sudo eopkg install vhba-module-current -y
-sudo eopkg install virtualbox-current -y
 
 
 
