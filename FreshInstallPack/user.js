56a7e8fbf1ebc2058ba70f11d19df5055d2acb9d
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-07 18:54:53 -0500

    Added numerous settings

--- FreshInstallPack/user.js
+++ FreshInstallPack/user.js
@@ -14,6 +14,9 @@ user_pref("config.trim_on_minimize", true);	// Default = false (and does not exi
 user_pref("browser.urlbar.clickSelectsAll", true);	// Default = false. Select all text when you click in the URL bar
 user_pref("security.dialog_enable_delay", 0);	// The amount of miliseconds to wait before allowing user to install new addon. Default = 1000
 user_pref("extensions.getAddons.maxResults", 25);	// Amount of results to display when searching for addons. Default = 15
+//user_pref("browser.download.dir", /home/rdogg/Desktop);	// Change the download directory
+user_pref("browser.startup.homepage", https://www.google.com/);	// Default homepage
+user_pref("browser.tabs.warnOnClose", false);	// Warn me before closing multiple tabs. Default = true
 
 
 // COMPATABILITY
@@ -26,13 +29,12 @@ user_pref("network.http.max-persistent-connections-per-server", 8);	// Max numbe
 
 
 // PRIVACY
-user_pref("dom.battery.enabled", false);	// /AllowPrevent website owners from tracking your computer's battery status. Default = true
+user_pref("dom.battery.enabled", false);	// AllowPrevent website owners from tracking your computer's battery status. Default = true
 user_pref("dom.event.clipboardevents.enabled", false);	// Allow/prevent websites from viewing what you copy/paste. Default = true
 //user_pref("media.navigator.enabled", false);	// Allow/prevent websites from tracking your microphone and camera connected to the PC. Default = true
-
-
-
-
+user_pref("privacy.trackingprotection.enabled", true);	// Enabled tracking protection. Deafult = false
+user_pref("privacy.donottrackheader.enabled", true);	// Send a "do not track" request when visiting sites. Default = false
+user_pref("app.shield.optoutstudies.enabled", false);	// Dis/Allows Mozilla from studying your PC. Default = true
 
 
 
