1c22edf294706b8ab80382fe0c872bb2e995096d
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-08 00:42:05 -0500

    Added user prompts to control what gets installed

--- FreshInstallPack/setup_solus.sh
+++ FreshInstallPack/setup_solus.sh
@@ -1,32 +1,59 @@
 #!/bin/sh
 
-echo run me as sudo -s
+BAKDIR=/home/$USER/SolusStarterBAKs
+mkdir $BAKDIR
+TODAY=`date '+%Y%m%d-%H%M'`
 
-# cerate a snapshot and save history log if a rollback is needed
+# Getting started
+echo You are logged in as $USER. For best results use your regular account.
+echo 
+
+while true; do
+    read -p "Install WiFi support? " yn
+    case $yn in
+        [Yy]* ) WIFI="install"; break;;
+        [Nn]* ) WIFI="skip"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Does your keyboard have custom buttons (ex: G keys)? " yn
+    case $yn in
+        [Yy]* ) G15="install"; break;;
+        [Nn]* ) G15="skip"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+
+# create a snapshot and save history log if a rollback is needed
 eopkg history -s
-eopkg history >/home/eopkghistory.log
+eopkg history >$BAKDIR/eopkghistory_$TODAY.log
 
 # list all default apps and save to list
-eopkg li >/home/defaultapps.lst
+eopkg li >$BAKDIR/defaultapps_$TODAY.lst
 
 # install support for my TPLink WN722Nv2 wifi adapter
-cp /etc/NetworkManager/NetworkManager.conf /home/NetworkManager.conf.BAK
-echo [device]>/etc/NetworkManager/NetworkManager.conf
-echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
+if [ $WIFI = "install" ]; then
+echo Backing up files before appending...
+sudo cp /etc/NetworkManager/NetworkManager.conf $BAKDIR/NetworkManager.conf.BAK-$TODAY
+sudo echo [device]>/etc/NetworkManager/NetworkManager.conf
+sudo echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
 echo YJRbLRRhPQ8844
+fi
 
 # reminders
 echo change the password to something easier with passwd
 echo if "clr-boot-manager update" has issues, add a line like this to fstab:
 echo UUID=1FE3-4160 /boot vfat defaults 0 0 
-echo I have already created a backup of fstab your desktop at /home/fstab.BAK
-cp /etc/fstab /home/fstab.BAK
+echo I have already created a backup of fstab your desktop at $BAKDIR/fstab.BAK-$TODAY
+cp /etc/fstab $BAKDIR/fstab.BAK-$TODAY
 echo Once /boot is mounted, run DoFlicky (Solus hardware detector) to install nvidia drivers
 
 # update and install things
-eopkg up
-eopkg install vim
-eopkg install nano
+sudo eopkg up
 
 # have user check kernel and drivers
 inxi -Fx
