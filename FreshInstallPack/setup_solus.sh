c7c6aa5bbc301403318a06a58c138bbad732614e
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-21 19:33:13 +0000

    Update setup_solus.sh

--- FreshInstallPack/setup_solus.sh
+++ FreshInstallPack/setup_solus.sh
@@ -2,19 +2,19 @@
 
 BAKDIR=/home/$USER/SolusStarterBAKs
 mkdir $BAKDIR
-TODAY=`date '+%Y%m%d-%H%M'`
+TODAYISO=`date '+%Y%m%d-%H%M'`
 
 # Getting started
 echo You are logged in as $USER. For best results use your regular account.
 echo 
 #if [ ir -r | grep root]="root"
 
-echo "Enter the UUID of your /boot drive "
+echo "Enter the UUID of your /boot drive: "
 read BOOTUUID
 echo You entered $BOOTUUID as the UUID. This is the wrong, restar this script.  
 
 while true; do
-    read -p "Install WiFi adapter support? " yn
+    read -p "Install WiFi adapter support? (y/n) " yn
     case $yn in
         [Yy]* ) WIFI="install"; break;;
         [Nn]* ) WIFI="skip"; break;;
@@ -23,7 +23,7 @@ while true; do
 done
 
 while true; do
-    read -p "Does your keyboard have custom buttons (ex: G keys)? " yn
+    read -p "Does your keyboard have custom buttons (ex: G keys)? (y/n) " yn
     case $yn in
         [Yy]* ) G15="install"; break;;
         [Nn]* ) G15="skip"; break;;
@@ -35,15 +35,15 @@ done
 
 # create a snapshot and save history log if a rollback is needed
 eopkg history -s
-eopkg history >$BAKDIR/eopkghistory_$TODAY.log
+eopkg history >$BAKDIR/eopkghistory_$TODAYISO.log
 
 # list all default apps and save to list
-eopkg li >$BAKDIR/defaultapps_$TODAY.lst
+eopkg li >$BAKDIR/defaultapps_$TODAYISO.lst
 
 # install support for my TPLink WN722Nv2 wifi adapter
 if [ $WIFI = "install" ]; then
 echo Backing up files before appending...
-sudo cp /etc/NetworkManager/NetworkManager.conf $BAKDIR/NetworkManager.conf.BAK-$TODAY
+sudo cp /etc/NetworkManager/NetworkManager.conf $BAKDIR/NetworkManager.conf.BAK-$TODAYISO
 sudo echo >>/etc/NetworkManager/NetworkManager.conf
 sudo echo [device]>>/etc/NetworkManager/NetworkManager.conf
 sudo echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
@@ -53,9 +53,9 @@ fi
 # Change fstab to reflect proper /boot drive.
 clr-boot-manager set-timeout 5
 clr-boot-manager update
-echo I have already created a backup of fstab your desktop at $BAKDIR/fstab.BAK-$TODAY
-cp /etc/fstab $BAKDIR/fstab.BAK-$TODAY
-sudo echo \# Added by $USER on $TODAY for kernel support >>/etc/fstab
+echo I have already created a backup of fstab your desktop at $BAKDIR/fstab.BAK-$TODAYISO
+cp /etc/fstab $BAKDIR/fstab.BAK-$TODAYISO
+sudo echo \# Added by $USER on $TODAYISO for kernel support >>/etc/fstab
 sudo echo UUID=$BOOTUUID /boot vfat defaults 0 0 >>/etc/fstab
 echo Once /boot is mounted, run DoFlicky (Solus hardware detector) to install nvidia drivers
 
