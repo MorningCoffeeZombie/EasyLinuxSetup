fabb95410036e0a0c3dbc9ee4097f04f9b092fa9
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-21 20:51:20 +0000

    Added ability to choose partition type and added fstab entries

--- FreshInstallPack/ssdSaver.sh
+++ FreshInstallPack/ssdSaver.sh
@@ -28,6 +28,29 @@ while true; do
     esac
 done
 
+if [ $SSDTMP = "install" ]; then
+	PS3='Enter your /tmp partition type: '
+	options=("vfat" "ext2" "ext3" "ext4" "ntfs" "Quit")
+	select partition in "${options[@]}"
+	do
+		case $partition in
+		"vfat")
+			TMPTYPE=$partition; echo "$partition entered"; break;;
+		"ext2")
+			TMPTYPE=$partition; echo "$partition entered"; break;;
+		"ext3")
+			TMPTYPE=$partition; echo "$partition entered"; break;;
+		"ext4")
+			TMPTYPE=$partition; echo "$partition entered"; break;;
+		"ntfs")
+			TMPTYPE=$partition; echo "$partition entered"; break;;
+		"Quit")
+			echo "Quiting program"; exit; break;;
+		*) echo "invalid option $REPLY";;
+		esac
+	done
+fi
+
 # SSD support for /var
 while true; do
     read -p "Boot /var from another drive (SSD support)? (y/n) " yn
@@ -37,6 +60,29 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+if [ $SSDVAR = "install" ]; then
+	PS3='Enter your /tmp partition type: '
+	options=("vfat" "ext2" "ext3" "ext4" "ntfs" "Quit")
+	select partition in "${options[@]}"
+	do
+		case $partition in
+		"vfat")
+			VARTYPE=$partition; echo "$partition entered"; break;;
+		"ext2")
+			VARTYPE=$partition; echo "$partition entered"; break;;
+		"ext3")
+			VARTYPE=$partition; echo "$partition entered"; break;;
+		"ext4")
+			VARTYPE=$partition; echo "$partition entered"; break;;
+		"ntfs")
+			VARTYPE=$partition; echo "$partition entered"; break;;
+		"Quit")
+			echo "Quiting program"; exit; break;;
+		*) echo "invalid option $REPLY";;
+		esac
+	done
+fi
 
 # Where will the / be? Used for editing the proper /etc/fstab.
 echo "Enter device name of the new (non-live) root system: "
@@ -52,10 +98,10 @@ read DEVICE
 echo 
 # Recap the entered variables
 echo You have entered the following: 
-echo /boot	$BOOTUUID
-echo /tmp	$TMPUUID
-echo /var	$VARUUID
-echo Root device	$DEVICE
+echo /boot	$BOOTUUID	vfat \(mandatory\)
+echo /tmp	$TMPUUID	$TMPTYPE
+echo /var	$VARUUID	$VARTYPE
+echo Device	$DEVICE
 echo 
 while true; do
     read -p "Is this correct? (y/n) " yn
@@ -71,21 +117,26 @@ done
 # MAKE ADDITIONS TO FSTAB
 #########################
 
-
 #sudo mount $DEVICE /run/media/live/
-#sudo echo \# Added $TODAYSTD during live-disk install to boot /tmp on separate drive>>/run/media/live/etc/fstab
+#sudo echo \# Added $TODAYSTD during live-disk install by ${0##*/}>>/run/media/live/etc/fstab
 
-# I NEED TO MAKE THIS THE FSTAB OF THE NONLIVE DISK!!!!!!!!!
-# need to find the file type of the /tmp drive (ext2, ext4, etc)
 # check the boot options are correct (0 0 or 0 1?)
 # also need to mount drive and rsync the files over
+#if [ $SSDBOOT = "install" ]; then
+#	sudo echo UUID=$BOOTUUID /boot vfat defaults 0 0 >>/run/media/live/etc/fstab
+	# vfat is hardcoded as UEFI boot must be vfat
+#fi
 #if [ $SSDTMP = "install" ]; then
-#	sudo echo UUID=$TMPUUID /tmp vfat defaults 0 0 >>/run/media/live/etc/fstab
+#	sudo echo UUID=$TMPUUID /tmp $TMPTYPE defaults 0 1 >>/run/media/live/etc/fstab
+#fi
+#if [ $SSDVAR = "install" ]; then
+#	sudo echo UUID=$VARUUID /var $VARTYPE defaults 0 1 >>/run/media/live/etc/fstab
 #fi
 
 
-
-
+##############################
+# RSYNC FILES TO NEW LOCATIONS
+##############################
 
 
 
@@ -102,4 +153,17 @@ exit
 ##################
 # UNUSED RESOURCES
 ##################
+
+
+#sudo blkid | grep $BOOTUUID
+
+
+
+
+
+
+
+
+
+
 
