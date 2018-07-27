ac17b1f694cf5c33e18695af912f21fcbe0c1531
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-07-26 21:19:37 -0500

    Added passwd change

--- FreshInstallPack/setupSolus.sh
+++ FreshInstallPack/setupSolus.sh
@@ -5,9 +5,19 @@ mkdir $BAKDIR
 TODAYISO=`date '+%Y%m%d-%H%M'`
 
 # Getting started
-echo You are logged in as $USER. For best results use your regular account.
-echo 
-#if [ ir -r | grep root]="root"
+if [ $USER = "root" ]; then
+	echo "It appears you are root. For best results do not start this script as root."
+	exit
+fi
+
+while true; do
+    read -p "Do you like your password? (y/n) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) CHNGPASS="true"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 while true; do
     read -p "Install WiFi adapter support? (y/n) " yn
@@ -28,6 +38,12 @@ while true; do
 done
 
 
+# change passwd per variable
+if [ $CHNGPASS = "true" ]; then
+	echo "Enter your current password to change it:"
+	sudo passwd $USER
+fi
+
 # create a snapshot and save history log if a rollback is needed
 eopkg history -s
 eopkg history >$BAKDIR/eopkghistory_$TODAYISO.log
@@ -35,23 +51,22 @@ eopkg history >$BAKDIR/eopkghistory_$TODAYISO.log
 # list all default apps and save to list
 eopkg li >$BAKDIR/defaultapps_$TODAYISO.lst
 
-# install support for my TPLink WN722Nv2 wifi adapter
+# install support for TPLink WN722Nv2 (or most other) wifi adapters
 if [ $WIFI = "install" ]; then
-echo Backing up files before appending...
-sudo cp /etc/NetworkManager/NetworkManager.conf $BAKDIR/NetworkManager.conf.BAK-$TODAYISO
-sudo echo >>/etc/NetworkManager/NetworkManager.conf
-sudo echo [device]>>/etc/NetworkManager/NetworkManager.conf
-sudo echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
-echo YJRbLRRhPQ8844
+	echo Backing up files before appending...
+	sudo cp /etc/NetworkManager/NetworkManager.conf $BAKDIR/NetworkManager.conf.BAK-$TODAYISO
+	sudo echo >>/etc/NetworkManager/NetworkManager.conf
+	sudo echo [device]>>/etc/NetworkManager/NetworkManager.conf
+	sudo echo wifi.scan-rand-mac-address=0>>/etc/NetworkManager/NetworkManager.conf
 fi
 
-# Change fstab to reflect proper /boot drive.
+# Backup fstab and prep for alt kernels
 clr-boot-manager set-timeout 5
 clr-boot-manager update
-echo I have already created a backup of fstab your desktop at $BAKDIR/fstab.BAK-$TODAYISO
 cp /etc/fstab $BAKDIR/fstab.BAK-$TODAYISO
+echo I have created a backup of fstab your desktop at $BAKDIR/fstab.BAK-$TODAYISO
 
-# update and install things
+# update OS
 sudo eopkg up
 
 # have user check kernel and drivers
@@ -66,5 +81,3 @@ inxi -Fx
 #################
 
 #echo change the password to something easier with passwd
-
-
