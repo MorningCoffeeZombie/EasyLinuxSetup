89175f68e69eda534278c32ada810e1e0c7588b4
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2018-10-26 18:25:39 -0500

    Added citation

--- SolusLinux/repoMigration.sh
+++ SolusLinux/repoMigration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # If your Solus installation or Live USB is out of date you may have noticed that you are unable to install/update from the original repo. This is due to a URL change from 'solus-project.com' to 'getsol.us'.
 # The below script will direct eopkg to the most recent repo (as of 11/2018)
+# https://getsol.us/2018/09/11/package-repo-migration-now-available/
 
 
 
