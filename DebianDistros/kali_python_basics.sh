4afc71f297ad5cf26e84fa52db3f64c53eabb807
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2019-10-21 12:01:47 -0500

    Python lessons

---
+++ DebianDistros/kali_python_basics.sh
@@ -0,0 +1,41 @@
+exit
+
+# INSTALL DEVELOPMENT RESOURCES
+###############################
+python --version	# Display python version installed on Kali Linux
+	# python 2.7 is the default for Kali as of 10/2019
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
